85b80c523844e254abc1bd8da13ea647f9bec8ac
gss <leeray0913>
2026-08-25 06:52:19 +0000

    Fix bad-fp.py

--- bad-fp.py
+++ bad-fp.py
@@ -30,9 +30,9 @@ if __name__ == "__main__":
     table_name = input("table name > ")
 
     try:
-        int(table_name)
-        if len(table_name) <= 5:
-            SELECT_ALL_FROM_TABLE(table_name)
+        TB = str(int(table_name))
+        if len(TB) <= 5:
+            SELECT_ALL_FROM_TABLE(TB)
         else:
             print("Table name is too long. Please enter a table name with 5 or fewer characters.")
     except ValueError:
